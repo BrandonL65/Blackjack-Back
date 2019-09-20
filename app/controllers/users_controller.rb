7ff1eadf283e7fdb142be4db3da207e5eabfce38
Brandon <brandon.lau86@gmail.com>
2019-09-19 21:00:03 -0400

    Ready to deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def create                                      #/signup
         newUser = User.create(user_params)                     
         if newUser.valid?                                           #if create user worked,
+            Chip.create(user: newUser, amount: 1000);
             payload = {user_id: newUser.id}                         #assign payload as the user's id 
             token = JWT.encode(payload, 'wework', 'HS256')          #create a token 
             render json: {token: token}                             #give token to JSON browser 
